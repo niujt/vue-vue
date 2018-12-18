b9d05c60ff226f82fab5829a40405190516c1bf4
niujt <670482466@qq.com>
2018-12-18 16:36:38 +0800

    新增表q

--- test.sql
+++ test.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50712
  File Encoding         : 65001
 
- Date: 17/12/2018 17:30:29
+ Date: 18/12/2018 16:36:12
 */
 
 SET NAMES utf8mb4;
@@ -36,5 +36,46 @@ INSERT INTO `city` VALUES (2, '浙江', '杭州');
 INSERT INTO `city` VALUES (3, '江苏', '无锡');
 INSERT INTO `city` VALUES (4, '上海', '徐汇区');
 INSERT INTO `city` VALUES (5, '北京', '三环');
+INSERT INTO `city` VALUES (6, '上海', '闵行区');
+INSERT INTO `city` VALUES (7, '广东', '深圳');
+
+-- ----------------------------
+-- Table structure for login
+-- ----------------------------
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE `login`  (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `userid` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of login
+-- ----------------------------
+INSERT INTO `login` VALUES (1, 'xiaoming', '123456', 1);
+INSERT INTO `login` VALUES (2, 'xiaohong', '123456', 2);
+INSERT INTO `login` VALUES (3, 'xiaogang', '123456', 3);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  `salary` decimal(10, 2) NULL DEFAULT NULL,
+  `addressid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '王小明', 18, 3000.00, '1');
+INSERT INTO `user` VALUES (2, '张小红', 22, 3500.00, '2');
+INSERT INTO `user` VALUES (3, '瞿小刚', 34, 4000.00, '3');
 
 SET FOREIGN_KEY_CHECKS = 1;
